85125fc71961b8579c3eb2c5f1b1dc700a8f8e7f
OhiaJanny <janechidera12@gmail.com>
2022-08-06 20:06:17 +0100

    Portf

---
+++ src/components/skill.js
@@ -0,0 +1,145 @@
+import React from 'react'
+
+const Skill = () => {
+    return (
+      <div
+        className="mx-auto"
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          width: "100%",
+          maxWidth: "1300px",
+        }}
+      >
+        <div className="container-fluid text-white">
+          <p className="slogan">
+            My <span id="text-other"> Experience</span>
+          </p>
+        </div>
+        <div style={{ width: "18rem" }}>
+          <div className="card-body">
+            <div className="accordion" id="accordionExample">
+              <div className="accordion-item">
+                <h2 className="accordion-header" id="headingOne">
+                  <button
+                    className="accordion-button"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseOne"
+                    aria-expanded="true"
+                    aria-controls="collapseOne"
+                  >
+                    Suretrip.ng Project
+                  </button>
+                </h2>
+                <div
+                  id="collapseOne"
+                  className="accordion-collapse collapse show"
+                  aria-labelledby="headingOne"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div className="accordion-body">
+                    <strong>UX/UI Expert.</strong> Lorem Ipsum is simply dummy
+                    text of the printing and typesetting industry. Lorem Ipsum
+                    has been the industry's standard dummy text ever since the
+                    1500s, when an unknown printer took a galley of type and
+                    scrambled it to make a type specimen book. It has survived
+                    not only five centuries, but also the leap into electronic
+                    typesetting, remaining essentially unchanged. It was
+                    popularised in the 1960s with the release of Letraset sheets
+                    containing Lorem Ipsum passages, and more recently with
+                    desktop publishing software like Aldus PageMaker including
+                    versions of Lorem Ipsum.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br />
+        <div style={{ width: "18rem" }}>
+          <div className="card-body">
+            <div className="accordion" id="accordionExample">
+              <div className="accordion-item">
+                <h2 className="accordion-header" id="headingTwo">
+                  <button
+                    className="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseTwo"
+                    aria-expanded="false"
+                    aria-controls="collapseTwo"
+                  >
+                    Kartlog.ng Project
+                  </button>
+                </h2>
+                <div
+                  id="collapseTwo"
+                  className="accordion-collapse collapse"
+                  aria-labelledby="headingTwo"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div className="accordion-body">
+                    <strong>UX/UI Expert</strong> Lorem Ipsum is simply dummy
+                    text of the printing and typesetting industry. Lorem Ipsum
+                    has been the industry's standard dummy text ever since the
+                    1500s, when an unknown printer took a galley of type and
+                    scrambled it to make a type specimen book. It has survived
+                    not only five centuries, but also the leap into electronic
+                    typesetting, remaining essentially unchanged. It was
+                    popularised in the 1960s with the release of Letraset sheets
+                    containing Lorem Ipsum passages, and more recently with
+                    desktop publishing software like Aldus PageMaker including
+                    versions of Lorem Ipsum.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div style={{ width: "18rem" }}>
+          <div className="card-body">
+            <div className="accordion" id="accordionExample">
+              <div className="accordion-item">
+                <h2 className="accordion-header" id="headingThree">
+                  <button
+                    className="accordion-button collapsed"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseThree"
+                    aria-expanded="false"
+                    aria-controls="collapseThree"
+                  >
+                    Triisum Project
+                  </button>
+                </h2>
+                <div
+                  id="collapseThree"
+                  className="accordion-collapse collapse"
+                  aria-labelledby="headingThree"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div className="accordion-body">
+                    <strong>UX/UI Expert</strong> Lorem Ipsum is simply dummy
+                    text of the printing and typesetting industry. Lorem Ipsum
+                    has been the industry's standard dummy text ever since the
+                    1500s, when an unknown printer took a galley of type and
+                    scrambled it to make a type specimen book. It has survived
+                    not only five centuries, but also the leap into electronic
+                    typesetting, remaining essentially unchanged. It was
+                    popularised in the 1960s with the release of Letraset sheets
+                    containing Lorem Ipsum passages, and more recently with
+                    desktop publishing software like Aldus PageMaker including
+                    versions of Lorem Ipsum.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Skill
